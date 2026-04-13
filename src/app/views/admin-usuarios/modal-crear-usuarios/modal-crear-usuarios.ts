3c9dde7040cf62c4f6ede8f366b12bb5d3e9c6e4
Mariana936 <73488291+Mariana936@users.noreply.github.com>
2026-04-12 19:51:32 -0500

    feat: improve user creation logic with role assignment and error handling

--- src/app/views/admin-usuarios/modal-crear-usuarios/modal-crear-usuarios.ts
+++ src/app/views/admin-usuarios/modal-crear-usuarios/modal-crear-usuarios.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { GestionUsuariosService } from '../../../services/gestion-usuarios';
+import { forkJoin, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-modal-crear-usuarios',
@@ -37,32 +38,58 @@ export class ModalCrearUsuarios implements OnInit {
   }
 
   guardar() {
+    if (!this.usuario || !this.nombre) {
+      alert('Debes llenar todos los campos');
+      return;
+    }
+
+    if (this.rolesSeleccionados.length === 0) {
+      alert('Debes seleccionar al menos un rol');
+      return;
+    }
 
     const fecha = new Date().toISOString().split('T')[0];
 
-    const requests = this.rolesSeleccionados.map(idRol => {
-      return this.usuariosService.crearUsuario({
-        nombreUsuario: this.usuario,
-        nombre: this.nombre,
-        id_rol: idRol,
+    // 🔹 1. CREAR USUARIO
+    this.usuariosService.crearUsuario({
+      nombreUsuario: this.usuario,
+      nombre: this.nombre
+    })
+    .pipe(
 
-        
-        //id_usuario: this.idUsuario,
-        //fechaAsignacion: fecha,
-        //estado: 'activo'
-      });
-    });
+      // 🔹 2. CUANDO SE CREA → ASIGNAR ROLES
+      switchMap((usuarioCreado: any) => {
 
-    // 🔥 ejecutar todos los requests
-    Promise.all(requests.map(r => r.toPromise()))
-      .then(() => {
-        console.log('✅ Roles asignados');
+        const idUsuario = usuarioCreado.id;
+
+        if (!idUsuario) {
+          console.error('❌ No vino el id del usuario');
+          return of(null);
+        }
+
+        const peticiones = this.rolesSeleccionados.map((idRol: string) =>
+          this.usuariosService.asignarRolUsuario({
+            id_usuario: idUsuario,
+            id_rol: idRol,
+            fechaAsignacion: fecha,
+            estado: 'Activo'
+          })
+        );
+
+        return forkJoin(peticiones);
+      })
+
+    )
+    .subscribe({
+      next: () => {
+        console.log('✅ Usuario creado con roles');
         this.guardado.emit();
         this.cerrar.emit();
-      })
-      .catch(() => {
-        console.error('❌ Error asignando roles');
-      });
+      },
+      error: (err) => {
+        console.error('❌ Error creando usuario con roles', err);
+      }
+    });
   }
 
   cerrarModal() {
